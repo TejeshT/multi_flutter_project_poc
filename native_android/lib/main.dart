1c1a9788d4a13ba9e512b5cc8d1156994b378d7f
Tejesh <thambatejesh118@gmail.com>
2024-10-01 16:24:03 +0530

    Imported demo_poc dart package

--- native_android/lib/main.dart
+++ native_android/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:demo_poc/demo_poc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -56,8 +57,10 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  List<dynamic> response = [];
 
-  void _incrementCounter() {
+  void _incrementCounter() async {
+    response = await fetchApiData(apiUrl: 'https://jsonplaceholder.typicode.com/posts');
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
@@ -105,8 +108,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Text(
+              response.isNotEmpty ? response[_counter -1]['title']
+              : 'You have pushed the button this many times:',
             ),
             Text(
               '$_counter',
