ed3517158f238065d9bda29a438bed352d4bf224
Tejesh <thambatejesh118@gmail.com>
2024-10-01 16:33:36 +0530

    Added dart package

---
+++ demo_poc/CHANGELOG.md
@@ -0,0 +1,6 @@
+## 1.0.0+4
+
+- Initial version.
+
+### Changed
+- Updated documentation for the readme.
---
+++ demo_poc/example/demo_poc_example.dart
@@ -0,0 +1,6 @@
+import 'package:demo_poc/demo_poc.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
